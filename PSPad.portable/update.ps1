4d0741ad94a65bc9cea4dee08331fbbc552f65d2
turboBasic <off@boun.cr>
2018-08-17 10:10:55 +0300

    WIP: Improve PSPad.portable

--- PSPad.portable/update.ps1
+++ PSPad.portable/update.ps1
@@ -21,11 +21,25 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 
-    $download_page = Invoke-WebRequest -Uri $releases
+    $downloadPage = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $regex = 'www\.pspad\.com/files/pspad/pspad.*en\.zip$'
+
+    $url = $downloadPage |
+        Select-Object -ExpandProperty links | 
+        Where-Object href -match $regex |
+        Select-Object -ExpandProperty href -First 1
 
 
-    $header = $download_page.ParsedHtml.getElementsByTagName('h2') | 
-        Where { $_.InnerHTML -match 'PSPad\s+-\s+current\s+version\s+(\d+\.\d+\.\d+)\s+\((\d+)\)' }
+    #$header = $download_page.ParsedHtml.getElementsByTagName('h2') | 
+    #    Where { $_.InnerHTML -match 'PSPad\s+-\s+current\s+version\s+(\d+\.\d+\.\d+)\s+\((\d+)\)' }
+
+    $html = New-Object -ComObject "HTMLFile"
+    $html.write( 
+        [System.Text.Encoding]::Unicode.GetBytes( $downloadPage.Content )
+    )
+    $html.getElementsByTagName('h2') | 
+        Select-Object -ExpandProperty innerHTML | 
+        Where-Object { $_ -match 'PSPad\s+-\s+current\s+version\s+(\d+\.\d+\.\d+)\s+\((\d+)\)' }
         
     $version = $Matches[1]
     $build   = $Matches[2]   
@@ -33,7 +47,7 @@ function global:au_GetLatest {
     
     $regexInstall = "\/files\/pspad($VersionShort)en\.zip$"
     
-    $urlInstall =  $download_page.links | 
+    $urlInstall =  $downloadPage.links | 
         Where href -match $regexInstall | 
         Select -First 1 -ExpandProperty href
     
@@ -41,7 +55,7 @@ function global:au_GetLatest {
     $fileName = Split-path $urlInstall -Leaf
     
     
-    $download_page.AllElements | 
+    $downloadPage.AllElements | 
         Where { 
             ( $_.tagName -eq 'CODE' ) -and 
             ( $_.innerText -match "([0-9A-F]{32})\s+(\S+.exe)\s+([0-9A-F]{32})\s+($fileName)" ) 
